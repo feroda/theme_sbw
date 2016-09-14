a3f2a5bd55ece63494bc1f7caaf691b2f45cb21e
Juho Jaakkola <juho@elgg.org>
2016-09-14 22:38:01 +0300

    Hides members section and footer from landing page

---
+++ views/default/landr/elements/members.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * This file removes the "Meet our members" section from landing
+ * page by overriding it with empty content.
+ */
---
+++ views/default/landr/footer.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * This file removes the footer section from landing
+ * page by overriding it with empty content.
+ */
